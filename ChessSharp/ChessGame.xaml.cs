c7d068d07ecaae7870440dec3a2d51b35cd6f740
Michael Easton <measton@my.apsu.edu>
2020-02-09 13:06:05 -0600

    Added GameLobby and ChessGame windows.

--- ChessSharp/ChessGame.xaml.cs
+++ ChessSharp/ChessGame.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ChessSharp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ChessGame.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChessGame : Window
     {
-        public MainWindow()
+        public ChessGame()
         {
             InitializeComponent();
         }
-
-       
     }
 }
--- ChessSharp/GameLobby.xaml.cs
+++ ChessSharp/GameLobby.xaml.cs
@@ -10,21 +10,30 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ChessSharp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameLobby.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameLobby : Window
     {
-        public MainWindow()
+        public GameLobby()
         {
             InitializeComponent();
         }
 
-       
+        private void gameButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            ChessGame cg = new ChessGame();
+            cg.Show();
+            this.Close();
+        }
+
+        private void logoutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
