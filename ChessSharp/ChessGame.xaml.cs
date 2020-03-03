0df276428dee94d3ce98616ecd9f2126d990442d
Alex Carrizo <acarrizo@my.apsu.edu>
2020-03-03 12:35:45 -0600

    fixing issue with chessgame window

--- ChessSharp/ChessGame.xaml.cs
+++ ChessSharp/ChessGame.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -264,5 +265,16 @@ namespace ChessSharp
 
 
         }
+
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+
+            ServerFunctions SV = new ServerFunctions();
+            SV.CloseGame();
+
+
+        }
+
     }
 }
